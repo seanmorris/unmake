d148c47d3c84f77f27b24b5201e59107c0dfda16
Sean Morris <sean@seanmorr.is>
2021-06-15 01:48:38 -0400

    Tweaks

--- bin/unmake-watch.sh
+++ bin/unmake-watch.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
+
+set -eu;
+set +x;
+
 inotifywait -qmre CREATE,DELETE,CLOSE_WRITE --format '%e %w%f' . | while read LINE; do {
 
-	echo "${LINE}";
+	echo "${LINE}" | grep -q ".unmake" && continue;
 
 	EVENT=$(echo ${LINE} | cut -d' ' -f1);
 	FILE=$(echo ${LINE} | cut -d' ' -f2);
 
 	INDEX=.unmake/index/${FILE#./}.unmak;
 
-	echo "${INDEX}";
-
-	[ "${EVENT#,CLOSE}" = "CLOSE_WRITE" ] && {
-		echo "Rebuilding index on ${EVENT%,CLOSE}...";
-		make unmake-index
+	[[ "${EVENT}" != "CLOSE_WRITE,CLOSE" ]] && {
+		echo "Rebuilding index on ${EVENT} ${FILE}.";
+		make unmake-index > /dev/null || true;
 	};
 
 	[[ -f ${INDEX} ]] || continue;
 
 	LIST=$(cat "${INDEX}");
 
-	echo "Rebuilding ${LIST}...";
-	[[ -f ${INDEX} ]] && make ${LIST};
+	echo "Rebuilding ${LIST} on ${EVENT} ${FILE}.";
+	[[ -f ${INDEX} ]] && make ${LIST} > /dev/null 2> /dev/null || true;
 
 }; done;
