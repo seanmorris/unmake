7972a84ad925f743633562ed2e1e963b49ef08db
Sean Morris <640101+seanmorris@users.noreply.github.com>
2022-01-13 08:37:41 -0500

    Ensuring the module scanner only scans one level.

--- bin/unmake-index.sh
+++ bin/unmake-index.sh
@@ -7,22 +7,15 @@ shopt -s dotglob extglob;
 rm -rf ./.unmake/unmake/index/!(.gitignore|..|.) || true;
 shopt -u dotglob extglob;
 
-find ./.unmake/modules/* -type d \
+find ./.unmake/modules/* -maxdepth 0 -type d \
 | while read MODULE; do {
-	
-	#echo -ne "${MODULE}\n" 1>&2;
-
 	bash "${MODULE}/list-sources.sh" \
 	| while read PREREQ; do {
 
 		mkdir -p $(dirname "./.unmake/unmake/index/${PREREQ}");
 
-		#echo -ne "\t${PREREQ}\n" 1>&2;
-
 		bash "${MODULE}/list-artifacts.sh" <<< "${PREREQ}" \
 		| while read TARGET; do {
-
-			#echo -ne "\t\t${TARGET}\n" 1>&2;
 
 			echo "${TARGET}" >> "./.unmake/unmake/index/${PREREQ}.unmak";
 
